23c689c45856e369a5ba9db66baf677710ba1862
YounesEch <114940175+YounesEch@users.noreply.github.com>
2023-02-24 08:36:06 +0100

    som js

---
+++ main.js
@@ -0,0 +1,39 @@
+const themeToggle = document.querySelector(".themeToggle");
+themeToggle.addEventListener('click', changeTheme);
+
+function changeTheme (){
+    document.body.classList.toggle('darkmode');
+    themeToggle.classList.toggle('darkmode');
+}
+
+const firstBtn = document.querySelector('#firstBtn');
+const secondBtn=document.querySelector('#secondBtn')
+const thirdBtn=document.querySelector('#thirdBtn')
+const forthBtn=document.querySelector('#forthBtn')
+
+
+firstBtn.addEventListener('click', goToUrl);
+secondBtn.addEventListener('click', goToSite);
+thirdBtn.addEventListener('click', goToPage);
+forthBtn.addEventListener('click', goToUrl1);
+
+
+function goToUrl(){
+    window.location.href="https://www.apple.com/se/iphone-14/";
+}
+
+function goToSite(){
+    window.location.href="https://www.samsung.com/pt/";
+}
+
+function goToPage(){
+    window.location.href="https://www.huawei.com/en/";
+}
+
+function goToUrl1(){
+    window.location.href="https://www.oneplus.com/global?gclid=4433e34f631413b9ce3780b82fe4db61&gclsrc=3p.ds&msclkid=4433e34f631413b9ce3780b82fe4db61";
+}
+
+
+
+
